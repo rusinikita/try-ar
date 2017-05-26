031345b0c0fc5c7cfcf14773ac6efede8b3d5462
Nikita <rusinikit@gmail.com>
2017-05-26 19:16:45 +0200

    Change MegaRenderer

--- app/src/main/kotlin/com/nikita/tryar/ar/MegaRenderer.kt
+++ app/src/main/kotlin/com/nikita/tryar/ar/MegaRenderer.kt
@@ -4,16 +4,32 @@ import android.opengl.GLSurfaceView
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
 
-class MegaRenderer : GLSurfaceView.Renderer {
-  override fun onDrawFrame(gl: GL10?) {
+class MegaRenderer(private val layers: List<RenderLayer>) : GLSurfaceView.Renderer {
+
+  override fun onSurfaceCreated(gl: GL10, config: EGLConfig) {
 
   }
 
-  override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
+  override fun onSurfaceChanged(gl: GL10, width: Int, height: Int) {
 
   }
 
-  override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
+  override fun onDrawFrame(gl: GL10) {
 
+  }
+
+  class RenderLayer {
+
+    fun onSurfaceCreated(gl: GL10, config: EGLConfig) {
+
+    }
+
+    fun onSurfaceChanged(gl: GL10, width: Int, height: Int) {
+
+    }
+
+    fun onDrawFrame(gl: GL10) {
+
+    }
   }
 }
